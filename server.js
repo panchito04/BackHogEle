d22c549f799198e89db577a3fada49a358f944f8
pancho040 <molinafranz943@gmail.com>
2026-05-28 10:02:39 -0400

    fix(cors): permitir origenes de capacitor en la configuracion de cors

--- server.js
+++ server.js
@@ -37,6 +37,8 @@ const corsOptions = {
       'http://localhost:5173',
       'http://localhost:5174',
       'http://localhost:3000',
+      'http://localhost',      // Capacitor Android
+      'capacitor://localhost',  // Capacitor iOS
       process.env.FRONTEND_URL
     ].filter(Boolean);
 
