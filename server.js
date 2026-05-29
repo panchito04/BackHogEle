22b997acf6b8f414ee8067b40a59c893428807ff
pancho040 <molinafranz943@gmail.com>
2026-05-28 22:55:25 -0400

    fix(cors): permitir origen https://localhost en la configuracion de cors

--- server.js
+++ server.js
@@ -37,7 +37,8 @@ const corsOptions = {
       'http://localhost:5173',
       'http://localhost:5174',
       'http://localhost:3000',
-      'http://localhost',      // Capacitor Android
+      'http://localhost',      // Capacitor Android (HTTP)
+      'https://localhost',     // Capacitor Android (HTTPS)
       'capacitor://localhost',  // Capacitor iOS
       process.env.FRONTEND_URL
     ].filter(Boolean);
